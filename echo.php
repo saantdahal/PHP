2dbcb8cec1e7834f0c93957b635e08632362dd43
Santosh Dahal <sirishsds@gmail.com>
2024-05-29 20:17:28 +0545

    Variables

--- echo.php
+++ echo.php
@@ -1,7 +1,10 @@
 <?php
+$name ="santosh";
+$age = 18;
+echo $name<br/>";
+echo $age;
 
-echo "Santosh Dahal<br/>";
-echo 18;
+echo "<br/>";
 
-echo("As a function");// 'echo'can be use at function
+// echo("As a function");// 'echo'can be use at function
 ?>
